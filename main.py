93b56f79b4a295c14b801f4236a48d2a06a02b03
Hamza <6793siddique@gmail.com>
2024-10-11 15:14:40 +0500

    Compliance Report

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ for x in data:
         destination_path = x.replace('destination_path = ', '').replace('\n', '')  
     if 'number_of_months_to_go_back' in x:
         number_of_months = int(x.replace('number_of_months_to_go_back = ', '').replace('\n', ''))  
+    if 'RCTIFile' in x:
+        rcti_file = x.replace('RCTIFile = ', '').replace('\n', '').lower() == 'true'
+    if 'ComplianceFile' in x:
+        compliance_file = x.replace('ComplianceFile = ', '').replace('\n', '').lower() == 'true'
 
 def login(page, context):
     time.sleep(1)
@@ -31,7 +35,7 @@ def login(page, context):
     else:
         print('Already Logged In')
 
-def save_file(content):
+def save_file(content, filename):
     downloaded_file_name = "downloaded.xlsx"
     with open(downloaded_file_name, 'wb') as f:
         f.write(content)
@@ -43,7 +47,7 @@ def save_file(content):
         return
 
     current_date_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = f"{destination_path}/alitsycertificatebillingcsv-{current_date_time}.csv"
+    file_name = f"{destination_path}/{filename}-{current_date_time}.csv"
     downloaded_file.to_csv(file_name, sep='|', index=False)
     print(f"File {file_name} downloaded successfully")
     os.remove(downloaded_file_name)
@@ -102,9 +106,32 @@ def post_request_with_saved_session(session, scheme_value, date_from, date_to):
         response = session.post(BASE_URL, data=payload)
         response.raise_for_status()
         print("Downloading the file...")
-        save_file(response.content)    
+        file_name = 'alitsycertificatebillingcsv'
+        save_file(response.content,file_name)    
     except requests.exceptions.RequestException as e:
         print(f"Request error: {e}")
+        
+def post_request_for_compliance_report(session, scheme_value, scheme_name, date_from, date_to):
+    print(scheme_value, date_from, date_to)
+    payload = {
+        'submitAction': 'ExportCsv',
+        'SchemeId': scheme_value,
+        'TypeOfDateFilter': 'Audit Assigned',
+        'DateFrom': date_from,
+        'DateTo': date_to,
+        'ExcludeRegistered': 'false'
+    }
+    
+    try:
+        # response = session.post(BASE_URL, data=payload)
+        # response.raise_for_status()
+        print("Downloading the file...")
+        file_name = 'alitsycompliancesummarycsv-'  + scheme_name
+        print(file_name)
+        return
+        save_file(response.content,file_name)    
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")        
 
 with sync_playwright() as playwright:
     
@@ -112,53 +139,70 @@ with sync_playwright() as playwright:
         (1, "ESS Lighting"),
         (43, "HEER HP"),
         (40, "HEER HVAC"),
-        (3, "HEER Lighting"),
-        (39, "IHEAB HP"),
-        (41, "IHEAB HVAC"),
-        (33, "IHEAB RDC"),
-        (5, "REPS CL"),
-        (37, "REPS HC2A"),
-        (44, "REPS HC2B"),
-        (42, "REPS HC3"),
-        (34, "REPS HP"),
-        (35, "REPS RDC1"),
-        (21, "SOLAR"),
-        (45, "STC HP - DO NOT USE"),
-        (32, "VEU APP"),
-        (31, "VEU HP"),
-        (4, "VEU RESI"),
-        (2, "VEU S34"),
-        (6, "VEU S35"),
-        (38, "VEU S44"),
-        (36, "VEU SH")
+        # (3, "HEER Lighting"),
+        # (39, "IHEAB HP"),
+        # (41, "IHEAB HVAC"),
+        # (33, "IHEAB RDC"),
+        # (5, "REPS CL"),
+        # (37, "REPS HC2A"),
+        # (44, "REPS HC2B"),
+        # (42, "REPS HC3"),
+        # (34, "REPS HP"),
+        # (35, "REPS RDC1"),
+        # (21, "SOLAR"),
+        # (45, "STC HP - DO NOT USE"),
+        # (32, "VEU APP"),
+        # (31, "VEU HP"),
+        # (4, "VEU RESI"),
+        # (2, "VEU S34"),
+        # (6, "VEU S35"),
+        # (38, "VEU S44"),
+        # (36, "VEU SH")
     ]
     
     date_ranges = get_month_date_ranges(number_of_months)
     
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="auth.json")
-    page = context.new_page()
-    page.goto(BASE_URL, wait_until="networkidle")
+    # browser = playwright.chromium.launch(headless=False)
+    # context = browser.new_context(storage_state="auth.json")
+    # page = context.new_page()
+    # page.goto(BASE_URL, wait_until="networkidle")
     
-    login(page, context)
-    time.sleep(2)
+    # login(page, context)
+    # time.sleep(2)
     
-    cookies = context.cookies()
-    session = requests.Session()
-    for cookie in cookies:
-        session.cookies.set(cookie['name'], cookie['value'], domain=cookie['domain'])
+    # cookies = context.cookies()
+    # session = requests.Session()
+    # for cookie in cookies:
+    #     session.cookies.set(cookie['name'], cookie['value'], domain=cookie['domain'])
     
-    page.get_by_label("Date Type").select_option("Audit Passed")
-    page.get_by_text("No", exact=True).click()
+    if rcti_file:
+        page.get_by_label("Date Type").select_option("Audit Passed")
+        page.get_by_text("No", exact=True).click()
 
-    for option in scheme_options:
-        option_value = int(option[0])
-        print(option[1])
-        select_scheme_option(page, option_value)
-        for start_date, end_date in date_ranges:
-            post_request_with_saved_session(session, option_value, start_date, end_date)
-        print('--------------------------------------------------------')
+        for option in scheme_options:
+            option_value = int(option[0])
+            print(option[1])
+            select_scheme_option(page, option_value)
+            for start_date, end_date in date_ranges:
+                post_request_with_saved_session(session, option_value, start_date, end_date)
+            print('--------------------------------------------------------')
+        print('------------------------RCTI Files Completed--------------------------------')
     
+    if compliance_file:
+        BASE_URL = 'https://ecovantage.alitsy.com/Report/ComplianceSummary'
+        page.goto(BASE_URL, wait_until="networkidle")
+        page.get_by_label("Date Type").select_option("Audit Assigned")
+
+        for option in scheme_options:
+            option_value = int(option[0])
+            option_name = option[1]
+            print(option[1])
+            select_scheme_option(page, option_value)
+            for start_date, end_date in date_ranges:
+                post_request_for_compliance_report(session, option_value, option_name, start_date, end_date)
+            print('--------------------------------------------------------')
+        print('------------------------Compliance Files Completed--------------------------------')    
+        
     time.sleep(2)
     context.close()
     browser.close()
